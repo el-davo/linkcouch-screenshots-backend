8a80a8214f4e1a447dcecdaeeb2ada72b63f270c
el-davo <david.ahern26@gmail.com>
2017-08-22 21:18:59 +0100

    Added read tokens endpoint

--- common/models/token.js
+++ common/models/token.js
@@ -2,7 +2,6 @@
 
 module.exports = Token => {
   Token.token = (req, next) => {
-    console.log(req);
     let AccessToken = Token.app.models.AccessToken;
 
     AccessToken.findForRequest(req, {}, (aux, accesstoken) => {
@@ -16,8 +15,25 @@ module.exports = Token => {
           ttl: -1,
           userId: accesstoken.userId,
           scopes: ['screenshots'],
-        }, (err, obj) => {
-          err ? next(err) : next(err, obj);
+        }, (err, token) => {
+          err ? next(err) : next(err, token);
+        });
+      });
+    });
+  };
+
+  Token.getUserTokens = (req, next) => {
+    let AccessToken = Token.app.models.AccessToken;
+
+    AccessToken.findForRequest(req, {}, (aux, accesstoken) => {
+      let UserModel = Token.app.models.User;
+      UserModel.findById(accesstoken.userId, (error) => {
+        if (error) {
+          return next(error);
+        }
+
+        AccessToken.find({where: {scopes: {inq: ['screenshots']}}}, (err, tokens) => {
+          err ? next(err) : next(err, tokens);
         });
       });
     });
@@ -27,6 +43,13 @@ module.exports = Token => {
       accepts: {arg: 'req', type: 'object', http: {source: 'req'}},
       returns: {arg: 'token', type: 'object'},
       http: {path: '/', verb: 'post'}
+    }
+  );
+
+  Token.remoteMethod('getUserTokens', {
+      accepts: {arg: 'req', type: 'object', http: {source: 'req'}},
+      returns: {arg: 'tokens', type: 'object'},
+      http: {path: '/', verb: 'get'}
     }
   );
 };
