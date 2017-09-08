3e855682fcdbccb2b317ccf740a2d6b35fbf8f8f
ahernd2 <david.ahern@emc.com>
2017-09-08 20:56:43 +0100

    Updating s3 config

--- common/config/config.js
+++ common/config/config.js
@@ -1,6 +1,10 @@
 let config = {
   selenium: {
     hub: process.env.seleniumHub || 'localhost'
+  },
+  aws: {
+    s3_endpoint: process.env.s3_endpoint,
+    port: process.env.s3_port
   }
 };
 
--- common/models/s3-validator.js
+++ common/models/s3-validator.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let config = require('../config/config');
+
 module.exports = S3Validator => {
   S3Validator.verifyS3Config = (req, s3Config, next) => {
     let AccessToken = S3Validator.app.models.AccessToken;
@@ -7,12 +9,12 @@ module.exports = S3Validator => {
     AccessToken.findForRequest(req, {}, () => {
       let AWS = require('aws-sdk');
 
-      AWS.config.update({
+      let s3 = new AWS.S3({
         accessKeyId: s3Config.key,
-        secretAccessKey: s3Config.secretKey
+        secretAccessKey: s3Config.secretKey,
+        s3ForcePathStyle: true,
+        endpoint: new AWS.Endpoint(`http://${config.aws.s3_endpoint}:${config.aws.port}`)
       });
-
-      let s3 = new AWS.S3();
 
       let s3Params = {
         Bucket: s3Config.bucket
